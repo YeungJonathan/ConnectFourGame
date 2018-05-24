a339a89af0c5febaee436a8352dbb9e05a675321
Jonathan Yeung <jonathanyeung1997@gmail.com>
2018-05-25 09:51:10 +1000

    Update symbol for Player class

--- connect4_player.py
+++ connect4_player.py
@@ -1,8 +1,12 @@
 import copy
 class player:
 	
-	def __init__(self, player_num = 1):
+	def __init__(self, player_num = 1, symbol):
 		self.__playerNum = player_num
+		self.__symbol = symbol
 	
 	def getPlayerNum(self):
 		return copy.deepcopy(self.__playerNum)
+	
+	def getSymbol(self):
+		return copy.deepcopy(self.__symbol)
